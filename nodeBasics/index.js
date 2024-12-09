ea904e872929c034d555dfbb104b7256db22b7c6
RASKELA <ss.corobruh@gmail.com>
2024-12-09 12:02:07 +0200

    Add chalk example in nodeBasics/index.js

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Привіт, це зелений текст!'));
+console.log(chalk.blue('А це синій текст!'));
+console.log(chalk.red.bold('Цей текст червоний і жирний!'));
